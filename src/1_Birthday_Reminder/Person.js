4878536869f3c6a6efb8074f6335a11eb5d46e1a
msa06 <suhaib.md06@gmail.com>
2021-05-18 21:45:57 +0530

    Added Remaining Days

--- src/1_Birthday_Reminder/Person.js
+++ src/1_Birthday_Reminder/Person.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Person = ({ person }) => {
+  const [daysRem, setRemDays] = useState(0);
+
+  useEffect(() => {
+    const calculateRemainingDays = () => {
+      let dob = new Date(person.dob);
+      let today = new Date();
+      let datetocome = new Date(
+        today.getFullYear(),
+        dob.getMonth(),
+        dob.getDay()
+      );
+      // One day in milliseconds
+      const oneDay = 1000 * 60 * 60 * 24;
+      const diffInTime = datetocome.getTime() - today.getTime();
+      const diffInDays = Math.round(diffInTime / oneDay);
+      console.log("Difference", diffInDays);
+      setRemDays(diffInDays);
+    };
+    calculateRemainingDays();
+  }, []);
+
   return (
     <div className="element">
       <img src={person.imgsrc} className="item-img" alt="person-img" />
       <div className="element-item">
         <h4>{person.name}</h4>
-        <h5>{person.age} years</h5>
+        <h4>{person.age} years</h4>
+        <h4>Days Remaining : {daysRem}</h4>
       </div>
     </div>
   );
